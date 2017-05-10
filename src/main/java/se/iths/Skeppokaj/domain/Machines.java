6457cbbd87039e5f1dd7e785212558c5ec75daea
Samuel Karlsson <Samuel Karlsson>
2017-05-10 11:48:27 +0200

    Skapat klassen Machines

---
+++ src/main/java/se/iths/Skeppokaj/domain/Machines.java
@@ -0,0 +1,13 @@
+package se.iths.Skeppokaj.domain;
+
+public class Machines {
+	
+	private int machineID;
+	private String lastName;
+	//private int id; //do we need this?
+	private int persId;
+	private String driversLicence;
+	private String status;
+	private String schemaType;
+
+}
