073d4deaeec1ee67948576603a693760c1d5e606
Samuel Karlsson <Samuel Karlsson>
2017-05-12 11:32:42 +0200

    Added unit test for Machines class

---
+++ src/test/java/se/iths/Skeppokaj/domain/MachinesTest.java
@@ -0,0 +1,35 @@
+package se.iths.Skeppokaj.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MachinesTest {
+
+		Machines test;
+		
+	@Before	
+	public void defineMachines() {
+		test = new Machines(0, null, null);
+	}
+	
+	@Test
+	public void MachineIdTest() {
+		test.setMachineID(1);
+		assertEquals("Test if machineID is set properly", 1, test.getMachineID());
+		
+	}
+	
+	@Test
+	public void MachineTypeTest() {
+		test.setMachineType("A001");
+		assertEquals("Test if machineType is set properly", "A001", test.getMachineType());
+	} 
+
+	@Test
+	public void MachineStatusTest() {
+		test.setMachineStatus("OK");
+		assertEquals("Test if machineStatus is set properly", "OK", test.getMachineStatus());
+	} 
+}
