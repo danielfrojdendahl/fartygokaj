061927939bc2b89f9bd48622bb930f8ce97063d2
Sofia <krakan_krax@hotmail.com>
2017-05-12 11:12:37 +0200

    Added unit test for Personnel class

---
+++ src/test/java/se/iths/Skeppokaj/domain/PersonnelTest.java
@@ -0,0 +1,68 @@
+package se.iths.Skeppokaj.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonnelTest {
+	
+	protected Personnel p;
+	
+	@Before
+	public void before(){
+		p = new Personnel("Harry","Hare",201,"A","Ledig","S");
+	}
+
+	@Test
+	public void testGetFirstName(){
+		assertEquals("Harry", p.getFirstName());
+	}
+	
+	@Test
+	public void testGetLastName(){
+		assertEquals("Hare", p.getLastName());
+	}
+	
+	@Test
+	public void testGetStatus(){
+		assertEquals("Ledig", p.getStatus());
+	}
+	
+	@Test
+	public void testGetPersId(){
+		assertEquals(201, p.getPersId());
+	}
+	
+	@Test
+	public void testGetDriversLicence(){
+		assertEquals("A", p.getDriversLicence());
+	}
+
+	@Test
+	public void testGetSchemaType(){
+		assertEquals("S", p.getSchemaType());
+	}
+	
+	@Test
+	public void testSetFirstName(){
+		String name = "Kalle";
+		p.setFirstName(name);
+		assertEquals(name, p.getFirstName());
+	}
+	
+	@Test
+	public void testSetLastName(){
+		String name = "Kula";
+		p.setLastName(name);
+		assertEquals(name, p.getLastName());
+	}
+	
+	@Test
+	public void testSetStatus(){
+		String status = "100%";
+		p.setStatus(status);
+		assertEquals(status, p.getStatus());
+	}
+	
+}
