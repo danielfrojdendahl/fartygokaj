0916aa6c02f8e650aa8a1970683a2da24b245c9e
Samuel Karlsson <Samuel Karlsson>
2017-05-12 11:25:18 +0200

    Update of Machines-class

--- src/main/java/se/iths/Skeppokaj/domain/Machines.java
+++ src/main/java/se/iths/Skeppokaj/domain/Machines.java
@@ -3,11 +3,58 @@ package se.iths.Skeppokaj.domain;
 public class Machines {
 	
 	private int machineID;
-	private String lastName;
-	//private int id; //do we need this?
-	private int persId;
-	private String driversLicence;
-	private String status;
-	private String schemaType;
+	private String machineType;
+	private String machineStatus;
+	
+	
+	public Machines(int machineID, String machineType, String machineStatus) {
+		super();
+		this.machineID = machineID;
+		this.machineType = machineType;
+		this.machineStatus = machineStatus;
+	}
+	
+	
+	
+	public int getMachineID() {
+		return machineID;
+	}
 
+
+
+	public void setMachineID(int machineID) {
+		this.machineID = machineID;
+	}
+	
+	
+	
+	public String getMachineType() {
+		return machineType;
+	}
+
+
+
+	public void setMachineType(String MachineType) {
+		this.machineType = MachineType;
+	}
+	
+	
+	
+	public String getMachineStatus() {
+		return machineStatus;
+	}
+
+
+
+	public void setMachineStatus(String machineStatus) {
+		this.machineStatus = machineStatus;
+	}
+	
+	
+	
+	@Override
+	public String toString(){
+		return new StringBuilder(machineType)  //how do we want a machine to be printed? just type?
+				.toString();
+	}
 }
