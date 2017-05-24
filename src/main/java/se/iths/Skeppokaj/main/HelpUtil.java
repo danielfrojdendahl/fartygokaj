45bc0ad5fc076c837048a6f6f879972df0e5408f
Samuel Karlsson <Samuel Karlsson>
2017-05-24 12:52:27 +0200

    changes to validName-method

--- src/main/java/se/iths/Skeppokaj/main/HelpUtil.java
+++ src/main/java/se/iths/Skeppokaj/main/HelpUtil.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class HelpUtil {
-	
+
 	final static List<String> machineTypes = Arrays.asList("A001", "AA01", "B001", "BB01", "C001", "CC01", "CCC1", "K001" );
 	final static List<String> driversLicenseType = Arrays.asList("A", "AA", "B", "BB", "C", "CC", "CCC", "K" );
 	final static List<String> statusType = Arrays.asList("100%", "50%", "0%", "Sjuk", "VAB", "Studier", "Semester");
 	final static List<String> schemaType = Arrays.asList("MF", "L", "S");
 	final static List<String> volumeType = Arrays.asList("A001", "AA01", "B001", "BB01", "C001", "CC01", "CCC1", "K001");
-	
-;	
+
+	;	
 	public static String validMachineType() {
 		String validatedMachineType = "";
 		boolean notInList = true;
@@ -26,7 +26,7 @@ public class HelpUtil {
 		}
 		return validatedMachineType;
 	}
-	
+
 	public static String validVolumeType() {
 		String validatedVolumeType = "";
 		boolean notInList = true;
@@ -39,57 +39,61 @@ public class HelpUtil {
 		}
 		return validatedVolumeType;
 	}
-	
+
 	public static String validDriversLicense() {
-	    String validatedDriversLicense = "";
-	    boolean notInList = true;
-	    while (notInList) {
-	        System.out.println("Godkända körtkortstyper är: ");
-	        System.out.println("A, AA, B, BB, C, CC, CCC, K");
-	        validatedDriversLicense = TextUtil.getReply("Körkortstyp: ");
-	        if (driversLicenseType.contains(validatedDriversLicense)) {
-	            notInList = false;
-	        }
-	    }
-	    return validatedDriversLicense;
+		String validatedDriversLicense = "";
+		boolean notInList = true;
+		while (notInList) {
+			System.out.println("Godkända körtkortstyper är: ");
+			System.out.println("A, AA, B, BB, C, CC, CCC, K");
+			validatedDriversLicense = TextUtil.getReply("Körkortstyp: ");
+			if (driversLicenseType.contains(validatedDriversLicense)) {
+				notInList = false;
+			}
+		}
+		return validatedDriversLicense;
 	}
-	
+
 	public static String validPersonalStatus() {
-	    String validatedPersonalStatus = "";
-	    boolean notInList = true;
-	    while (notInList) {
-	        System.out.println("Godkänd status för personal är: ");
-	        System.out.println("100%, 50%, 0%, Sjuk, VAB, Studier, Semester");
-	        validatedPersonalStatus = TextUtil.getReply("Status: ");
-	        if (statusType.contains(validatedPersonalStatus)) {
-	            notInList = false;
-	        }
-	    }
-	    return validatedPersonalStatus;
+		String validatedPersonalStatus = "";
+		boolean notInList = true;
+		while (notInList) {
+			System.out.println("Godkänd status för personal är: ");
+			System.out.println("100%, 50%, 0%, Sjuk, VAB, Studier, Semester");
+			validatedPersonalStatus = TextUtil.getReply("Status: ");
+			if (statusType.contains(validatedPersonalStatus)) {
+				notInList = false;
+			}
+		}
+		return validatedPersonalStatus;
 	}
 
 	public static String validSchemaType() {
-	    String validatedSchemaType = "";
-	    boolean notInList = true;
-	    while (notInList) {
-	        System.out.println("Tillåtna scheman är: ");
-	        System.out.println("MF, L, S");
-	        validatedSchemaType = TextUtil.getReply("Schema: ");
-	        if (schemaType.contains(validatedSchemaType)) {
-	            notInList = false;
-	        }
-	    }
-	    return validatedSchemaType;
+		String validatedSchemaType = "";
+		boolean notInList = true;
+		while (notInList) {
+			System.out.println("Tillåtna scheman är: ");
+			System.out.println("MF, L, S");
+			validatedSchemaType = TextUtil.getReply("Schema: ");
+			if (schemaType.contains(validatedSchemaType)) {
+				notInList = false;
+			}
+		}
+		return validatedSchemaType;
 	}
-	
-	
+
+
 	public static String validName() {
-		String validatedName = TextUtil.getReply("Namn: ");    //method with a scanner
-		while (validatedName.matches(".*\\d.*")) { 		       //check if contains digigts
+		boolean notValidName = true;
+		String validatedName = "";    				
+		while (notValidName) { 		       			
+			validatedName = TextUtil.getReply("Namn: ");
+			notValidName = validatedName.matches(".*\\d.*");
+			if (notValidName == true) {
 				System.out.println("Namn kan inte innehålla siffror!");
-				validatedName = TextUtil.getReply("Namn: ");
+			}
 		}
 		return validatedName;
 	}
-	
+
 }
