fbf5ef8fa668c06559fcb96d3b13722717765988
mikaela <mikaela.jenas@hotmail.com>
2017-06-13 11:50:25 +0200

    wq

--- src/test/java/se/iths/Skeppokaj/db/DBStorageTest.java
+++ src/test/java/se/iths/Skeppokaj/db/DBStorageTest.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import se.iths.Skeppokaj.domain.Machines;
 import se.iths.Skeppokaj.domain.Personnel;
+import se.iths.Skeppokaj.domain.Ships;
 
 public class DBStorageTest {
 
@@ -72,4 +74,78 @@ public class DBStorageTest {
 			fail("GetPersonnelByID failed");
 		}
 	}
+	
+	@Test
+	public void addMachineTest() {
+		Machines m = new Machines(10, "AA01", "Ok", 1000);
+		assertTrue(storage.addMachine(m));
+	}
+
+	@Test
+	public void addMachineVoidTests() {
+		Machines m = new Machines(10, "000", "G", 1000);
+		assertFalse(storage.addMachine(m));
+	}
+
+	@Test
+	public void deleteMachineTest() {
+		Machines m = new Machines(56, "AA01", "Ok", 1000);
+		if(storage.addMachine(m)){
+			assertTrue(storage.deleteMachine(m));
+		}else{
+			fail("DeleteMachine failed");
+		}
+}
+	
+	@Test
+	public void updateStatusMachineTest() {
+		Machines m = new Machines(40,"AA01", "Ok",1000);
+		if(storage.addMachine(m)){
+			m.setMachineStatus("Skadad");
+			assertTrue(storage.updateStatusMachine(m));
+		}else{
+			fail("UpdateStatusMachinefailed");
+		}
+	}
+	
+	@Test
+	public void  getMachineByID() {
+		Machines m = new Machines(59,"AA01", "Reserv", 1000);
+		List<Machines> list;
+		if(storage.addMachine(m)){
+			list = storage.getMachinesByID(59);
+			//assertTrue(list.contains(m));
+		}else{
+			fail("GetMachineByID failed");
+			}
+		}
+	@Ignore
+	@Test
+	public void addShipTest() {
+		Ships s = new Ships(102, "Bepa", "DHL", "A005");
+		assertTrue(storage.addShip(s));
+	}
+	@Ignore
+	@Test
+	public void deleteShipTest() {
+		Ships s = new Ships(102, "Bepa", "DHL", "A005");
+		if(storage.addShip(s)){
+			assertTrue(storage.deleteShip(s));
+		}else{
+			fail("DeleteShip failed");
+		}
+	}
+	
+	public void  getShipByID() {
+		Ships s = new Ships(19,"Diana", "Odio Tristique Limited", "A005");
+		List<Ships> list;
+		if(storage.addShip(s)){
+			list = storage.getShipsByID(19);
+			assertTrue(list.contains(s));
+		}else{
+			fail("GetShipByID failed");
+		}
+	}
+
+	
 }
